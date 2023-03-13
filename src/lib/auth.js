827ee239e30ba9a5d9aeff03e81e1f2c3ecae779
roskata729 <roskata728@gmail.com>
2023-03-13 10:58:08 +0200

    Restrict adding events and lessons to administrators only.

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,4 @@
 module.exports = {
-
     isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
@@ -12,6 +11,15 @@ module.exports = {
             return next();
         }
         return res.redirect('/profile');
-    }
+    },
 
+    isAdministrator(req, res, next){
+        if(req.isAuthenticated()){
+            if(req.user.is_admin){
+                return next();
+            }
+            return res.redirect('/profile');
+        }
+        return res.redirect('/signin');
+    }
 }
