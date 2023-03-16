e216e033b77ba047fe42d73977627d40c37176ba
roskata729 <roskata728@gmail.com>
2023-03-16 17:50:54 +0200

    Make the js logic in the sign up page separated from the hbs file

---
+++ src/public/js/signup.js
@@ -0,0 +1,18 @@
+const usernameInput = document.getElementById('username');
+const usernameInfo = document.getElementById('username-info');
+const registerForm = document.querySelector('form[action="/signup"]');
+
+registerForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`/check-username/${usernameInput.value}`);
+
+    if (response.ok) {
+        registerForm.submit();
+    } else {
+        usernameInput.style.borderColor = 'red';
+        usernameInput.style.animation = 'shake .5s';
+        usernameInput.style.animationIterationCount = 'infinite';
+        usernameInfo.innerText = 'Потребителското име е заето';
+    }
+});
